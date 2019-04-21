6bf1e8c7494005092845132c23d76253b2f167b5
Shubham Soni <shubham.soni@jungleworks.co>
2019-04-21 20:15:21 +0530

    Addition Screen

--- app/src/main/java/com/mathgame/AdditionActivity.java
+++ app/src/main/java/com/mathgame/AdditionActivity.java
@@ -2,17 +2,36 @@ package com.mathgame;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.sasank.roundedhorizontalprogress.RoundedHorizontalProgressBar;
 
-public class AdditionActivity extends BaseActivity {
+public class AdditionActivity extends BaseActivity implements View.OnClickListener {
     private RoundedHorizontalProgressBar pbTimer;
+    private ImageView                    ivBack;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addition);
-        pbTimer=findViewById(R.id.pbTimer);
+        pbTimer = findViewById(R.id.pbTimer);
+        ivBack = findViewById(R.id.ivBack);
         pbTimer.animateProgress(8000, 0, 100);
+        Utils.setOnClickListener(this, ivBack);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ivBack:
+                onBackPressed();
+                break;
+        }
+    }
+    @Override
+    public void onBackPressed() {
+        Transition.exit(this);
     }
 }
