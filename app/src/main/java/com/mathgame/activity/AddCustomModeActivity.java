60cdf8bb660d6185b24754bc89b601648a90cadd
Shubham Soni <shubham.soni@jungleworks.co>
2019-04-23 15:11:36 +0530

    Add custom mode

--- app/src/main/java/com/mathgame/activity/AddCustomModeActivity.java
+++ app/src/main/java/com/mathgame/activity/AddCustomModeActivity.java
@@ -2,18 +2,41 @@ package com.mathgame.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.AppCompatTextView;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 
-import com.mathgame.structure.BaseActivity;
 import com.mathgame.R;
+import com.mathgame.plugin.numberpicker.NumberPicker;
+import com.mathgame.structure.BaseActivity;
+import com.mathgame.util.Transition;
 import com.mathgame.util.Utils;
+import com.rey.material.widget.CheckBox;
+import com.rey.material.widget.RadioButton;
 
 public class AddCustomModeActivity extends BaseActivity implements View.OnClickListener {
-    private RelativeLayout rlChallengeType;
-    private Spinner        spnCustomFieldValues;
+    private RelativeLayout     rlChallengeType;
+    private Spinner            spnCustomFieldValues;
+    private AppCompatImageView ivBack;
+    private AppCompatTextView  tvTitle;
+    private NumberPicker       npNumberOfQuestion;
+    private NumberPicker       npNumberOfVariables;
+    private NumberPicker npNumberOfSkip;
+    private RadioButton        rbSinglePlayer;
+    private RadioButton        rbDualPlayer;
+    private RadioButton        rbTimerNone;
+    private RadioButton        rbTimerPerTest;
+    private RadioButton        rbTimerPerQuestion;
+    private CheckBox           cbAddition;
+    private CheckBox           cbSubtraction;
+    private CheckBox           cbMultiplication;
+    private CheckBox           cbDivision;
+    private CheckBox           cbPercentage;
+    private CheckBox           cbSquareRoot;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,7 +50,7 @@ public class AddCustomModeActivity extends BaseActivity implements View.OnClickL
         String[] dropDownItems = new String[3];
         dropDownItems[0] = getString(R.string.muliple_choice);
         dropDownItems[1] = getString(R.string.yes_no);
-        dropDownItems[2] =getString(R.string.manual_input);
+        dropDownItems[2] = getString(R.string.manual_input);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, dropDownItems);
         spnCustomFieldValues.setAdapter(adapter);
     }
@@ -35,7 +58,23 @@ public class AddCustomModeActivity extends BaseActivity implements View.OnClickL
     private void init() {
         rlChallengeType = findViewById(R.id.rlChallengeType);
         spnCustomFieldValues = findViewById(R.id.spnCustomFieldValues);
-        Utils.setOnClickListener(this, rlChallengeType);
+        tvTitle = findViewById(R.id.tvTitle);
+        npNumberOfQuestion = findViewById(R.id.npNumberOfQuestion);
+        ivBack = findViewById(R.id.ivBack);
+        rbSinglePlayer = findViewById(R.id.rbSinglePlayer);
+        rbDualPlayer = findViewById(R.id.rbDualPlayer);
+        cbAddition = findViewById(R.id.cbAddition);
+        cbSubtraction = findViewById(R.id.cbSubtraction);
+        cbMultiplication = findViewById(R.id.cbMultiplication);
+        cbDivision = findViewById(R.id.cbDivision);
+        cbPercentage = findViewById(R.id.cbPercentage);
+        cbSquareRoot = findViewById(R.id.cbSquareRoot);
+        npNumberOfVariables = findViewById(R.id.npNumberOfVariables);
+        rbTimerNone = findViewById(R.id.rbTimerNone);
+        rbTimerPerTest = findViewById(R.id.rbTimerPerTest);
+        npNumberOfSkip=findViewById(R.id.npNumberOfSkip);
+        rbTimerPerQuestion = findViewById(R.id.rbTimerPerQuestion);
+        Utils.setOnClickListener(this, rlChallengeType, ivBack);
     }
 
     @Override
@@ -44,6 +83,14 @@ public class AddCustomModeActivity extends BaseActivity implements View.OnClickL
             case R.id.rlChallengeType:
                 spnCustomFieldValues.performClick();
                 break;
+            case R.id.ivBack:
+                onBackPressed();
+                break;
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        Transition.exit(this);
     }
 }
--- app/src/main/java/com/mathgame/activity/CustomModeActivity.java
+++ app/src/main/java/com/mathgame/activity/CustomModeActivity.java
@@ -2,20 +2,27 @@ package com.mathgame.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.mathgame.R;
 import com.mathgame.adapter.CustomModeAdapter;
 import com.mathgame.model.CustomMode;
 import com.mathgame.structure.BaseActivity;
+import com.mathgame.util.Transition;
+import com.mathgame.util.Utils;
 
 import java.util.ArrayList;
 
-public class CustomModeActivity extends BaseActivity {
-    private RecyclerView rvCustomModeList;
-    private CustomModeAdapter adapter;
+public class CustomModeActivity extends BaseActivity implements View.OnClickListener {
+    private AppCompatImageView    ivBack;
+    private RecyclerView          rvCustomModeList;
+    private CustomModeAdapter     adapter;
     private ArrayList<CustomMode> customModes;
+    private AppCompatImageView    ivAddOption;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +32,33 @@ public class CustomModeActivity extends BaseActivity {
     }
 
     private void setList() {
-        adapter=new CustomModeAdapter(customModes);
+        adapter = new CustomModeAdapter(customModes);
         rvCustomModeList.setAdapter(adapter);
     }
 
     private void init() {
-        rvCustomModeList=findViewById(R.id.rvCustomModeList);
+        rvCustomModeList = findViewById(R.id.rvCustomModeList);
+        ivBack = findViewById(R.id.ivBack);
+        ivAddOption = findViewById(R.id.ivAddOption);
         rvCustomModeList.setLayoutManager(new LinearLayoutManager(this));
+        Utils.setOnClickListener(this, ivBack);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ivBack:
+                onBackPressed();
+                break;
+            case R.id.ivAddOption:
+                Transition.startActivity(this, AddCustomModeActivity.class);
+                break;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        Transition.exit(this);
     }
 }
