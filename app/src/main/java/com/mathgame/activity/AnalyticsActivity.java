fda290665f684200849e8a13cdfa9cccbe321017
Shubham Soni <shubham.soni@jungleworks.co>
2019-05-11 13:35:12 +0530

    Game Result in Progress

--- app/src/main/java/com/mathgame/activity/AnalyticsActivity.java
+++ app/src/main/java/com/mathgame/activity/AnalyticsActivity.java
@@ -3,17 +3,13 @@ package com.mathgame.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
-import com.github.mikephil.charting.charts.PieChart;
 import com.mathgame.R;
 import com.mathgame.structure.BaseActivity;
 
-public class GameResultActivity extends BaseActivity {
-    private PieChart pieChartPlayerOne;
+public class AnalyticsActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_result);
-        pieChartPlayerOne=findViewById(R.id.pieChartPlayerOne);
-
+        setContentView(R.layout.activity_analytics);
     }
 }
---
+++ app/src/main/java/com/mathgame/model/Result.java
@@ -0,0 +1,75 @@
+package com.mathgame.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Result implements Parcelable {
+    private int total;
+    private int correct;
+    private int incorrect;
+    private int unAttempted;
+
+    protected Result(Parcel in) {
+        total = in.readInt();
+        correct = in.readInt();
+        incorrect = in.readInt();
+        unAttempted = in.readInt();
+    }
+
+    public static final Creator<Result> CREATOR = new Creator<Result>() {
+        @Override
+        public Result createFromParcel(Parcel in) {
+            return new Result(in);
+        }
+
+        @Override
+        public Result[] newArray(int size) {
+            return new Result[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(total);
+        dest.writeInt(correct);
+        dest.writeInt(incorrect);
+        dest.writeInt(unAttempted);
+    }
+    public int getTotal() {
+        return total;
+    }
+
+    public int getCorrect() {
+        return correct;
+    }
+
+    public int getIncorrect() {
+        return incorrect;
+    }
+
+    public int getUnAttempted() {
+        return unAttempted;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public void setCorrect(int correct) {
+        this.correct = correct;
+    }
+
+    public void setIncorrect(int incorrect) {
+        this.incorrect = incorrect;
+    }
+
+    public void setUnAttempted(int unAttempted) {
+        this.unAttempted = unAttempted;
+    }
+
+}
