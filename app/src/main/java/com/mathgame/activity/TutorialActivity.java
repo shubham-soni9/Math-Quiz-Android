4caf8e6c1b90192f7997e781bdfb59a9e3711909
Shubham Soni <shubham.soni@jungleworks.co>
2019-05-12 19:46:32 +0530

    Adde

---
+++ app/src/main/java/com/mathgame/activity/TutorialActivity.java
@@ -0,0 +1,15 @@
+package com.mathgame.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.mathgame.R;
+import com.mathgame.structure.BaseActivity;
+
+public class TutorialActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial);
+    }
+}
